376a10274f48c5763a360361dedb1ed647b3cc33
Andreas-Wall <m.andreas.wall@protonmail.com>
2021-07-01 11:09:20 -0500

    changed url

--- src/service/StudentService.jsx
+++ src/service/StudentService.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const baseurl = 'http://studentapi-env.eba-eunpxuhj.us-east-1.elasticbeanstalk.com/';
+const baseurl = 'http://ec2-34-231-241-107.compute-1.amazonaws.com';
 
 // custom hook for performing GET request
 export const useFetch = (searchMethod, searchValue) => {
